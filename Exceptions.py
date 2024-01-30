f65f5ddf5811c8119371bd783b7fafafab607a22
Teurus <darkozavr@gmail.com>
2024-01-30 23:25:58 +0500

    Неполное описание внутренней логики и исключений. Пометки комментариями на исправления

--- Exceptions.py
+++ Exceptions.py
@@ -7,10 +7,15 @@ class BoardOutException(Exception):
         return f'Точка ({self.dot}) -> {self.msg}'
 
 
-class ShipDirectionError(Exception):
-    def __init__(self, direction, msg='направление указано не верно'):
+class ShipDirectionError(Exception):    # МОЖНО УДАЛИТЬ, ПЕРЕДЕЛАТЬ direction НА boolean
+    def __init__(self, direction, msg='направление указано неверно'):
         self.direction = direction
         self.msg = msg
 
     def __str__(self):
         return f'{self.direction} - {self.msg}'
+
+
+class ShipPlacementError(Exception):
+    def __init__(self, msg='невозможно поставить'):
+        self.msg = msg
--- InnerLogic.py
+++ InnerLogic.py
@@ -3,33 +3,59 @@
 
 import Exceptions
 
+
 class Dot:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
 
     def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+        return self._x == other.x and self._y == other.y
 
     def __str__(self):
-        return f'{self.x}, {self.y}'
+        return f'{self._x}, {self._y}'
+
+    def get_dot(self):
+        return self._x, self._y
 
 
 class Ship:
-    def __init__(self, length, head_dot, direction, life):
-        self.length = length
-        self.head_dot = head_dot
-        self.direction = direction
-        self.life = life
+    def __init__(self, length, head_dot, direction):
+        self._length = length
+        self._head_dot = head_dot
+        self._direction = direction
+        self._life = length
 
     def dots(self):
-        dot_list = [self.head_dot]
-        for i in range(1, self.length):
-            if self.direction == 'vertical':
-                dot_list.append(Dot(self.head_dot.x, self.head_dot.y + i))
-            elif self.direction == 'horizontal':
-                dot_list.append(Dot(self.head_dot.x + i, self.head_dot.y))
+        dot_list = [self._head_dot]
+        for i in range(1, self._length):
+            if self._direction == 'vertical':  # ПЕРЕДЕЛАТЬ ЧЕРЕЗ boolean
+                dot_list.append(Dot(self._head_dot.x, self._head_dot.y + i))
+            elif self._direction == 'horizontal':
+                dot_list.append(Dot(self._head_dot.x + i, self._head_dot.y))
             else:
-                raise
+                raise Exceptions.ShipDirectionError(self._direction)
+
+    def get_damage(self):
+        self._life -= 1
+
+    def get_head_dot(self):
+        return self._head_dot
+
+    def get_direction(self):
+        return self._direction
 
 
+class Board:
+    _status = [[], [], [], [], [], []]
+    _ships_list = []
+    _hid = True
+    _ships_count = None
+
+    def __init__(self):
+        for row in self._status:
+            for i in range(6):
+                row.append('O')
+
+    def add_ship(self, length, dot, direction):
+        ...
